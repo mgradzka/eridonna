cc1a4d0315d988acd73b217edabfd7f55f3d292f
Martyna227 <gradzkamartyna@gmail.com>
2023-06-18 14:12:02 +0200

    final changes

--- assets/application.js
+++ assets/application.js
@@ -1,4 +1,4 @@
-// Put your application javascript here
+// scroll up option
 
 window.addEventListener('scroll', function() {
   const scrollToTopButton = document.getElementById('scrollToTopButton');
@@ -19,6 +19,8 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
+// sorting
+
 if (document.getElementById("sort_by") != null) {
   document.querySelector("#sort_by").addEventListener("change", function (e) {
     const url = new URL(window.location.href);
@@ -28,34 +30,8 @@ if (document.getElementById("sort_by") != null) {
   });
 }
 
-if (document.getElementById("country") != null) {
-  document.getElementById("country").addEventListener("change", function (e) {
-    const provinces =
-      this.options[this.selectedIndex].getAttribute("data-provinces");
-    const provinceSelector = document.getElementById("province");
-    const provinceArray = JSON.parse(provinces);
+// forgotten password
 
-    // console.log(provinceArray);
-    if (provinceArray.length < 1) {
-      provinceSelector.setAttribute("disabled", "disabled");
-    } else {
-      provinceSelector.removeAttribute("disabled");
-    }
-
-    provinceSelector.innerHTML = "";
-    let options = "";
-    for (let i = 0; i < provinceArray.length; i++) {
-      options +=
-        '<option value="' +
-        provinceArray[i][0] +
-        '">' +
-        provinceArray[i][0] +
-        "</option>";
-    }
-
-    provinceSelector.innerHTML = options;
-  });
-}
 
 if (document.getElementById("forgotPassword") != null) {
   document.getElementById("forgotPassword").addEventListener("click", (e) => {
@@ -70,32 +46,6 @@ if (document.getElementById("forgotPassword") != null) {
   });
 }
 
-// const productModal = new bootstrap.Modal(
-//   document.getElementById("productInfoModal"),
-//   {}
-// );
-
-// const productInfoAnchors = document.querySelectorAll("#productInfoAnchor");
-// if (productInfoAnchors.length > 0) {
-//   productInfoAnchors.forEach((item) => {
-//     item.addEventListener("click", (event) => {
-//       const url = '/products/' + item.getAttribute('product-handle') + '.js';
-//       fetch(url)
-//         .then((resp) => resp.json())
-//         .then(function (data) {
-//           console.log(data);
-
-//           document.getElementById('productInfoImg').src = data.images[0];
-//           document.getElementById('productInfoTitle').innerHTML = data.title;
-//           document.getElementById('productInfoPrice').innerHTML = item.getAttribute('product-price');
-//           document.getElementById('productInfoDescription').innerHTML = data.description;
-
-//           productModal.show();
-//         });
-
-//     });
-//   });
-// }
 
 if (document.getElementById("forgotPasswordAccount") != null) {
   document.getElementById("forgotPasswordAccount").addEventListener("click", (e) => {
@@ -109,6 +59,20 @@ if (document.getElementById("forgotPasswordAccount") != null) {
     }
   });
 }
+
+// nav box shadow
+window.addEventListener('scroll', function() {
+  const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollPosition > 100) { 
+    navbar.classList.add('scrolled-nav');
+  } else {
+    navbar.classList.remove('scrolled-nav');
+  }
+});
+
+
+// updating cart
 
 document.addEventListener("DOMContentLoaded", (data) => {
   updateCart(data);
@@ -125,7 +89,7 @@ async function updateCart(data) {
   }
 }
 
-// menu
+// navbar
 const menuElm = document.querySelector(".navbar-toggler");
 const icon = document.querySelector(".navbar-toggler-icon");
 const fontawesome = document.getElementById("mobile");
@@ -141,6 +105,7 @@ menuElm.addEventListener("click", function (event) {
 });
 
 
+// filters
 
 const filterContainer = document.querySelector(".filter-container");
 const filterIcon = document.getElementById("filter-icon");
@@ -161,20 +126,7 @@ if (filterIcon) {
   });
 }
 
-// const generalDescription = document.querySelector('.general-description')
-// const technicalInformation = document.querySelector('.technical-description')
-// const generalText = document.querySelector('.general-description-text')
-// const technicalText = document.querySelector('.technical-description-text')
-
-// if (generalDescription) {
-//   generalDescription.addEventListener('click', (e) => {
-//     if (generalText.className.indexOf("display-none") === -1) {
-//       generalText.classList.add("display-none");
-//     } else {
-//       generalText.classList.remove("display-none");
-//     }
-//   })
-// }
+// brand page button toggler
 
 const allButton = document.getElementById("allButton");
 const watchesButton = document.getElementById("watchesButton");
@@ -197,7 +149,7 @@ if (allButton && watchesButton && jewelryButton) {
   });
 }
 
-// Check if the articles exist before performing operations on them
+// Checking if the articles exist before performing operations on them
 const articles = document.getElementsByClassName("article");
 
 if (articles) {
@@ -240,9 +192,12 @@ function setActiveButton(button) {
 
 setActiveButton(allButton)
 
+
 const productPage = document.querySelector("#productPage");
 
 if (productPage != null) {
+
+  // mobile slider
   const slider = document.querySelector(".slider");
   const prevButton = document.querySelector(".prev-button");
   const nextButton = document.querySelector(".next-button");
@@ -290,6 +245,7 @@ if (productPage != null) {
     console.log(currentValue);
   });
 
+  // sticky gallery
   window.addEventListener("scroll", function () {
     const container = document.querySelector(".product-page-container");
     const imageWrapper = document.querySelector(".product-image-wrapper");
@@ -308,36 +264,22 @@ if (productPage != null) {
   });
 
 
-
-
 }
 
 const collectionPage = document.querySelector(".collection-page");
 const navbar = document.querySelector(".navbar");
 const mainContent = document.querySelector("main");
 if (collectionPage) {
+  // navbar not sticky on collection page
   navbar.classList.remove("fixed-top");
   mainContent.classList.remove("margin-top");
 }
-
-
-// nav box shadow
-window.addEventListener('scroll', function() {
-  const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-
-  if (scrollPosition > 100) { // Change 100 to the appropriate scroll position where you want to change the navbar color
-    navbar.classList.add('scrolled-nav');
-  } else {
-    navbar.classList.remove('scrolled-nav');
-  }
-});
 
 
 // account page - dynamic content
 const accountButtons = document.querySelectorAll("#btn-account");
 const accountContent = document.querySelectorAll("[id^='accountcontent']");
 const spantext =  document.querySelectorAll(".span-account")
-// const button3 = document.getElementById("button3");
 
 for (let i = 0; i < accountButtons.length; i++) {
   accountButtons[i].addEventListener("click", function() {
